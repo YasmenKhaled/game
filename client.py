8e68022fc5433ee9c4eff840bf10930d54458080
Yasmen <92948115+YasmenKhaled@users.noreply.github.com>
2023-05-31 00:38:03 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,32 +1,97 @@
-import threading
 import socket
-alias = input('Choose an alias >>> ')
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 59000))
+import threading
+import tkinter as tk
 
+class Client:
+    def __init__(self, master):
+        self.master = master
+        self.username = None
+        self.socket = None
 
-def client_receive():
-    while True:
-        try:
-            message = client.recv(1024).decode('utf-8')
-            if message == "alias?":
-                client.send(alias.encode('utf-8'))
-            else:
-                print(message)
-        except:
-            print('Error!')
-            client.close()
-            break
+        self.message_frame = tk.Frame(master)
+        self.message_frame.pack()
 
+        self.scrollbar = tk.Scrollbar(self.message_frame)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-def client_send():
-    while True:
-        message = f'{alias}: {input("")}'
-        client.send(message.encode('utf-8'))
+        self.message_list = tk.Listbox(self.message_frame, yscrollcommand=self.scrollbar.set)
+        self.message_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
+        self.scrollbar.config(command=self.message_list.yview)
 
-receive_thread = threading.Thread(target=client_receive)
-receive_thread.start()
+        self.input_frame = tk.Frame(master)
+        self.input_frame.pack()
 
-send_thread = threading.Thread(target=client_send)
-send_thread.start()
+        self.username_entry = tk.Entry(self.input_frame, width=10)
+        self.username_entry.pack(side=tk.LEFT)
+
+        self.connect_button = tk.Button(self.input_frame, text="Connect", command=self.connect)
+        self.connect_button.pack(side=tk.LEFT)
+
+        self.send_button = tk.Button(self.input_frame, text="Send", command=self.send_message, state=tk.DISABLED)
+        self.send_button.pack(side=tk.RIGHT)
+
+        self.input_entry = tk.Entry(self.input_frame, width=50, state=tk.DISABLED)
+        self.input_entry.pack(side=tk.RIGHT)
+
+    def connect(self):
+        self.username = self.username_entry.get().strip()
+        if not self.username:
+            return
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(("localhost", 8000))
+
+        threading.Thread(target=self.receive_messages).start()
+
+        self.username_entry.config(state=tk.DISABLED)
+        self.connect_button.config(state=tk.DISABLED)
+        self.input_entry.config(state=tk.NORMAL)
+        self.send_button.config(state=tk.NORMAL)
+
+        self.send_message("{} has joined the chat.".format(self.username))
+
+    def receive_messages(self):
+        while True:
+            try:
+                message = self.socket.recv(1024).decode()
+                if not message:
+                    break
+
+                self.message_list.insert(tk.END, message)
+            except ConnectionResetError:
+                break
+
+        self.disconnect()
+
+    def send_message(self, message=None):
+        if not message:
+            message = self.input_entry.get().strip()
+            self.input_entry.delete(0, tk.END)
+
+        if message:
+            self.socket.sendall(message.encode())
+
+    def disconnect(self):
+        self.socket.close()
+
+        self.username_entry.config(state=tk.NORMAL)
+        self.connect_button.config(state=tk.NORMAL)
+        self.input_entry.config(state=tk.DISABLED)
+        self.send_button.config(state=tk.DISABLED)
+
+        self.message_list.insert(tk.END, "Disconnected from server.")
+
+    def on_close(self):
+        self.disconnect()
+
+    def run(self):
+        self.master.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.master.mainloop()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Chat Room Client")
+
+    client = Client(root)
+    client.run()
--- server.py
+++ server.py
@@ -1,53 +1,89 @@
-'Chat Room Connection - Client-To-Client'
-import threading
 import socket
-host = '127.0.0.1'
-port = 59000
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
-clients = []
-aliases = []
+import threading
+import tkinter as tk
 
+class Server:
+    def __init__(self, master, port):
+        self.master = master
+        self.port = port
+        self.socket = None
+        self.clients = {}
 
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+        self.status_label = tk.Label(master, text="Server is running on port {}".format(port))
+        self.status_label.pack()
 
-# Function to handle clients'connections
+        self.start_button = tk.Button(master, text="Start", command=self.start_server)
+        self.start_button.pack()
 
+        self.stop_button = tk.Button(master, text="Stop", command=self.stop_server, state=tk.DISABLED)
+        self.stop_button.pack()
 
-def handle_client(client):
-    while True:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            alias = aliases[index]
-            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
-            aliases.remove(alias)
-            break
-# Main function to receive the clients connection
+    def start_server(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(("localhost", self.port))
+        self.socket.listen(5)
 
+        self.status_label.config(text="Server is running on port {}".format(self.port))
+        self.start_button.config(state=tk.DISABLED)
+        self.stop_button.config(state=tk.NORMAL)
 
-def receive():
-    while True:
-        print('Server is running and listening ...')
-        client, address = server.accept()
-        print(f'connection is established with {str(address)}')
-        client.send('alias?'.encode('utf-8'))
-        alias = client.recv(1024)
-        aliases.append(alias)
-        clients.append(client)
-        print(f'The alias of this client is {alias}'.encode('utf-8'))
-        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
-        client.send('you are now connected!'.encode('utf-8'))
-        thread = threading.Thread(target=handle_client, args=(client,))
-        thread.start()
+        threading.Thread(target=self.accept_clients).start()
 
+    def accept_clients(self):
+        while True:
+            client_socket, address = self.socket.accept()
+
+            username = "User{}".format(len(self.clients) + 1)
+            client_socket.sendall(username.encode())
+
+            client = {"username": username, "socket": client_socket}
+            self.clients[username] = client
+
+            self.broadcast_message("{} has joined the chat.".format(username))
+
+            threading.Thread(target=self.handle_client, args=(client,)).start()
+
+    def handle_client(self, client):
+        username = client["username"]
+        socket = client["socket"]
+
+        while True:
+            try:
+                message = socket.recv(1024).decode()
+                if not message:
+                    break
+
+                self.broadcast_message("{}: {}".format(username, message))
+            except ConnectionResetError:
+                break
+
+        self.remove_client(username)
+
+    def remove_client(self, username):
+        client = self.clients.pop(username)
+        client["socket"].close()
+
+        self.broadcast_message("{} has left the chat.".format(username))
+
+    def broadcast_message(self, message):
+        for client in self.clients.values():
+            client["socket"].sendall(message.encode())
+
+    def stop_server(self):
+        for client in self.clients.values():
+            client["socket"].close()
+
+        self.socket.close()
+        self.socket = None
+
+        self.status_label.config(text="Server stopped.")
+        self.start_button.config(state=tk.NORMAL)
+        self.stop_button.config(state=tk.DISABLED)
 
 if __name__ == "__main__":
-    receive()
+    root = tk.Tk()
+    root.title("Chat Room Server")
+
+    server = Server(root, 8000)
+
+    root.mainloop()
